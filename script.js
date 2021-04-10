c95501213ceb08f48e90a5ca531955be1ab9e961
HemantNasa <71221999+HemantNasa@users.noreply.github.com>
2021-04-10 21:54:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,73 +6,71 @@ let browserPromise = pup.launch({
     defaultViewport: false
 });
 let sitelink = "https://www.covid19india.org/";
-let state = "India";
+let state = "India";  // can be changed for different states
 
-let headingarr = [];
-let detailsarrfinal = [];
-let detailsarr = [];
-let tab;
-let ans = {};
-let statearr = [];
-let count = 0;
+let headingarr = []; // array made to contain all headings
+let detailsarrfinal = []; // final details about particular state
+let detailsarr = []; // details of all states
+let tab; // on which we will be working
+let ans = {}; // Final answer will be stored in it
+let statearr = []; // names of states
+let count = 0; // To count the Ranking of the state among others in terms of total confirmed covid cases
 browserPromise.then(function(browser){
     let pagesPromise = browser.pages();
-    return pagesPromise;
+    return pagesPromise; // promise regarding to bring addresses of all tabs open
 }).then(function(pages){
-    tab = pages[0];
-    let pageOpenPromise = tab.goto(sitelink);
-    return pageOpenPromise;
+    tab = pages[0]; // first tab open
+    let pageOpenPromise = tab.goto(sitelink); // visint the site from where we will extract data
+    return pageOpenPromise; 
 }).then(function(){
-    let waitPromise = tab.waitForSelector(".cell.heading", {visible : true});
+    let waitPromise = tab.waitForSelector(".cell.heading", {visible : true}); // waiting for the visibility of headings to put in headingarr... Line(11)
     return waitPromise;
 }).then(function(){
-    let allheadingsPromise = tab.$$(".cell.heading");
+    let allheadingsPromise = tab.$$(".cell.heading"); // promise to have all the headings with this unique class
     return allheadingsPromise;
 }).then(function(data){
     for(let i of data) {
         let innertext = tab.evaluate(function(ele){
-            return ele.textContent;
+            return ele.textContent; // promise of extracting inner element from the data for headings
         },i);
         headingarr.push(innertext);
     }
     return Promise.all(headingarr);
 }).then(function(data){
-    headingarr = data;
-    let allstatesPromise = tab.$$(".state-name.fadeInUp");
+    headingarr = data; // all heading we got...  Line(11)
+    let allstatesPromise = tab.$$(".state-name.fadeInUp"); // to get states name
     return allstatesPromise;
 }).then(function(data){
     for(let i of data){
         let innertext = tab.evaluate(function(ele){
-            return ele.textContent;
+            return ele.textContent; // promise of extracting inner element from the data for states name
         },i);
         statearr.push(innertext);
     }
     return Promise.all(statearr);
 }).then(function(data){
     // console.log(data);
-    count = data.indexOf(state);
+    count = data.indexOf(state); // We got the ranking of the state all over India in terms of total confirmed covid cases.... Line(17)
     // console.log(count);
     return data;
 }).then(function(data){
-    let allstatesPromise = tab.$$(".state-name.fadeInUp");
+    let allstatesPromise = tab.$$(".state-name.fadeInUp"); // promises of treversing over states to collect the covid details of all states
     return allstatesPromise;
 }).then(function(data){
     for(let i in data){
-        if(i==count){
-            let detailsPromise = tab.$$(".cell.statistic .total");
+            let detailsPromise = tab.$$(".cell.statistic .total"); // promises of all specific details for all states
             return detailsPromise;
-        }
     }
 }).then(function(data){
     for(let i of data){
         let innertext = tab.evaluate(function(ele){
-            return ele.textContent;
+            return ele.textContent; // got inner content for all the promises int data
         },i);
         detailsarr.push(innertext);
     }
     return Promise.all(detailsarr);
 }).then(function(data){
-    detailsarr = data;
+    detailsarr = data; 
     let start = (count)*6;
     let end = (count+1)*6-1;
     for(let k = start;k<=end;k++){
